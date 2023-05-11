baa05649817d55ccbedfc435f7a1758df1b170a9
mathis47000 <mathisbescond3@gmail.com>
2023-05-11 16:14:19 +0200

    un

--- lib/widgets/house/full_screen_image.dart
+++ lib/widgets/house/full_screen_image.dart
@@ -10,7 +10,7 @@ class FullScreenImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Image'),
+        title: const Text('Imagesssss'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
